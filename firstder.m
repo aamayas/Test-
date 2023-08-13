fc214855e225b038e8ec701bb5680dd5c1157d52
cwinant <clinton.winant@gmail.com>
2023-08-13 10:19:59 +0100

    Revert "Add files via upload"

--- firstder.m
+++
@@ -1,6 +0,0 @@
-function derivative=firstder(x,dx);
-  derivative=zeros(size(x));
-  derivative(2:end-1)=(x(3:end)-x(1:end-2))/2/dx;
-  derivative(1)=(-3*x(1)+4*x(2)-x(3))/2/dx;
-  derivative(end)=(3*x(end)-4*x(end-1)+x(end-2))/2/dx;
-endfunction
